4cdb3286e393425169c7a39bb223dfb093288e88
Alejandro Berbin <alejandro.berbin@yesdevs.com>
2026-02-21 14:03:15 +0100

    feat: Add Vercel Analytics and Speed Insights, and introduce 'Clear Filters' translation strings.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -41,6 +41,9 @@ export async function generateMetadata({
   };
 }
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default async function LocaleLayout({
   children,
   params,
@@ -71,6 +74,8 @@ export default async function LocaleLayout({
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Analytics />
+            <SpeedInsights />
           </ThemeProvider>
         </NextIntlClientProvider>
       </body>
